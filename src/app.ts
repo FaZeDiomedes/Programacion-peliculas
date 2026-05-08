e81be0e54e81eb732166df7cac289cd97cf88194
Tu Nombre <tuemail@example.com>
2026-05-07 19:51:58 -0500

    Version server selector

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import express from "express";
 import cors from "cors";
 import compression from "compression";
 import helmet from "helmet";
+
 import v1Routes from "./api/v1/index";
-import { errorMiddleware } from "./middlewares/error.middleware";
+
 import peliculasRoutes from "./modules/peliculas/peliculas.routes";
 import reviewsRoutes from "./modules/reviews/reviews.routes";
 import watchLaterRoutes from "./modules/watchLater/watchLater.routes";
+
 import swaggerUi from "swagger-ui-express";
 import { openApiSpec } from "./config/openapi";
+
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 export const app = express();
 
@@ -17,13 +21,15 @@ app.use(cors());
 app.use(compression());
 app.use(express.json());
 
+app.get("/saludar", (_req, res) => {
+  res.json({ Saludar: "Hola" });
+});
 
-//Prueba de rutas app.ts
-app.get('/saludar', (_req, res) => {
-    res.json({"Saludar": "Hola"})
-})
-
-app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(openApiSpec));
+app.use(
+  "/api/v1/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(openApiSpec)
+);
 
 app.use("/api/v1/peliculas", peliculasRoutes);
 app.use("/api/v1", v1Routes);
@@ -31,3 +37,9 @@ app.use("/api/v1/watch-later", watchLaterRoutes);
 app.use("/api/v1/reviews", reviewsRoutes);
 
 app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
